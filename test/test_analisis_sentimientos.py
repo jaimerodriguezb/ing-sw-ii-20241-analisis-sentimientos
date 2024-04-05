6a08476c062f32026ea9d930e0ee5dcd8bdb196c
lorcarlos <carperez45@uan.edu.co>
2024-04-04 19:46:07 -0500

    modelo funcionando

--- test/test_analisis_sentimientos.py
+++ test/test_analisis_sentimientos.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import MagicMock
-from codigo_produccion import  AnalisisSentimientos
+from modelo.codigo_produccion import  AnalisisSentimientos
 import pandas as pd
 import numpy as np
 import joblib
@@ -27,9 +27,24 @@ def test_comportamiento_negativo():
     resultado = test.clasificar_sentimiento(sentimiento)
     assert resultado == "Mensaje negativo"
 
+#inicial las pruebas con el modelo
+@pytest.fixture
+def positive_text(): 
+    return "@AmericanAir Joanne from your San Diego staff was phenomenal! Give that girl a raise. She handled our #flightnightmare better than anyone."
 
+@pytest.fixture
+def negative_text():
+    return "@USAirways The airline is embarrassing itself. I get that bad weather isn't your fault, but your response to it couldn't have been worse."
 
-        
+@pytest.fixture
+def neutral_text():
+    return "@VirginAmerica plz help me win my bid upgrade for my flight 2/27 LAX---&gt;SEA!!!  🍷👍💺✈️"
+
+#modelor
+def test_comportamiento_neutral_modelo(neutral_text):
+    test =  AnalisisSentimientos()
+    resultado = test.clasificar_sentimiento(neutral_text)
+    assert resultado == "Mensaje neutro"
 
 
 
